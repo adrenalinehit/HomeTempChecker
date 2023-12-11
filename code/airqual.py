536e4bf6b14a5245c0045f447f68cbb2ff84dc2b
Nick <adrenalinehit@users.noreply.github.com>
2023-12-11 21:11:38 +0000

    airqual

--- code/airqual.py
+++ code/airqual.py
@@ -9,26 +9,20 @@ pmaqi = FluentMetric().with_namespace("AirQual").with_stream_id("aqi")
 
 ser = serial.Serial('/dev/ttyUSB0')
 
-try:
-    while True:
-      data = []
-      
-      for index in range(0,10):
-          datum = ser.read()
-          data.append(datum)
-      
-      pmtwo = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
-      pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
-      myaqi = aqi.to_aqi([(aqi.POLLUTANT_PM25, str(pmtwo)),
-                          (aqi.POLLUTANT_PM10, str(pmten))])
-      aqi = float(myaqi)
-      
-      pm2.log(MetricName="CurrentPM25", Value=pmtwo, Unit="None")
-      pm10.log(MetricName="CurrentPM10", Value=pmten, Unit="None")
-      pmaqi.log(MetricName="CurrentAQI", Value=aqi, Unit="None")
-      
-      time.sleep(300)
-      
-except KeyboardInterrupt:
-    pass
-  
+data = []
+
+for index in range(0,10):
+    datum = ser.read()
+    data.append(datum)
+
+pmtwo = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
+pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
+myaqi = aqi.to_aqi([(aqi.POLLUTANT_PM25, str(pmtwo)),
+                    (aqi.POLLUTANT_PM10, str(pmten))])
+aqi = float(myaqi)
+
+pm2.log(MetricName="CurrentPM25", Value=pmtwo, Unit="None")
+pm10.log(MetricName="CurrentPM10", Value=pmten, Unit="None")
+pmaqi.log(MetricName="CurrentAQI", Value=aqi, Unit="None")
+
+time.sleep(300)
