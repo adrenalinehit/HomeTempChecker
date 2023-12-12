ee0bd57558641ed5bf4a4b9959e187240c369c6f
Nick <adrenalinehit@users.noreply.github.com>
2023-12-12 13:04:07 +0000

    Update templogger.py for crontab running

--- code/templogger.py
+++ code/templogger.py
@@ -3,8 +3,6 @@ from fluentmetrics import FluentMetric
 
 m = FluentMetric().with_namespace("HomeTemp").with_stream_id("HomeTemp")
 
-try:
-    while True:
         tempfile = open("/sys/bus/w1/devices/28-0000073e3ff8/w1_slave")
         thetext = tempfile.read()
         tempfile.close()
@@ -14,7 +12,3 @@ try:
         m.log(MetricName="CurrentTemp", Value=temperature, Unit="None")
         # print temperature
         # print "logged"
-
-        time.sleep(300)
-except KeyboardInterrupt:
-    pass
