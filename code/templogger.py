8e14ec4c43ef4254ad4f7c2462fc339e0146d631
Nick <adrenalinehit@users.noreply.github.com>
2023-12-12 13:04:57 +0000

    Update templogger.py

--- code/templogger.py
+++ code/templogger.py
@@ -3,12 +3,10 @@ from fluentmetrics import FluentMetric
 
 m = FluentMetric().with_namespace("HomeTemp").with_stream_id("HomeTemp")
 
-        tempfile = open("/sys/bus/w1/devices/28-0000073e3ff8/w1_slave")
-        thetext = tempfile.read()
-        tempfile.close()
-        tempdata = thetext.split("\n")[1].split(" ")[9]
-        temperature = float(tempdata[2:])
-        temperature = temperature / 1000
-        m.log(MetricName="CurrentTemp", Value=temperature, Unit="None")
-        # print temperature
-        # print "logged"
+tempfile = open("/sys/bus/w1/devices/28-0000073e3ff8/w1_slave")
+thetext = tempfile.read()
+tempfile.close()
+tempdata = thetext.split("\n")[1].split(" ")[9]
+temperature = float(tempdata[2:])
+temperature = temperature / 1000
+m.log(MetricName="CurrentTemp", Value=temperature, Unit="None")
